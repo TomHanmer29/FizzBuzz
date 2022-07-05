d4af959b7ea53316bd17112c041550272c642950
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-05 13:54:05 +0100

    Created a working version of fizzbuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace FizzBuzz
+﻿using System.Text;
+
+namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int caseChecker = 0;
+            for (int currentNo = 1; currentNo <= 100; currentNo++)
+            {
+                StringBuilder fizzBuzzOutput = new StringBuilder();
+                if (IsMultiple(currentNo, 3))
+                {
+                    fizzBuzzOutput.Append("Fizz");
+                }
+
+                if (IsMultiple(currentNo, 5))
+                {
+                    fizzBuzzOutput.Append("Buzz");
+                }
+
+                if (fizzBuzzOutput.Equals(""))
+                {
+                    Console.WriteLine(currentNo);
+                }
+                else
+                {
+                    Console.WriteLine(fizzBuzzOutput);
+                }
+            }
+        }
+
+        private static bool IsMultiple(int inputNo, int multiplier)
+        {
+            return (inputNo % multiplier == 0);
         }
     }
 }
