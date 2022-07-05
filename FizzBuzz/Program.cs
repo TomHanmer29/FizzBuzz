37752103c8e2a1fc6da948f9cca988a7cc22d1cf
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-05 15:11:15 +0100

    Added special rules up to 13, just the 17 special rule left to program. Also rewrote lots of the code to neaten it up!

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,17 +6,16 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int currentNo = 1; currentNo <= 100; currentNo++)
+            int maxFizzBuzz = EnterNo();
+            int[] fizzBuzzCheckers = { 3, 5, 7, 11, 13, 17 };
+            string[] fizzBuzzWords = { "Fizz", "Buzz", "Bang", "Bong", "Fezz", "placeholder"};
+            for (int currentNo = 1; currentNo <= maxFizzBuzz; currentNo++)
             {
                 StringBuilder fizzBuzzOutput = new StringBuilder();
-                if (IsMultiple(currentNo, 3))
+                for(int iterator = 0; iterator<fizzBuzzCheckers.Length; iterator++ )
                 {
-                    fizzBuzzOutput.Append("Fizz");
-                }
-
-                if (IsMultiple(currentNo, 5))
-                {
-                    fizzBuzzOutput.Append("Buzz");
+                    fizzBuzzOutput = FizzBuzzAppend(fizzBuzzOutput, fizzBuzzWords[iterator], currentNo,
+                        fizzBuzzCheckers[iterator]);
                 }
 
                 if (fizzBuzzOutput.Equals(""))
@@ -27,12 +26,73 @@ namespace FizzBuzz
                 {
                     Console.WriteLine(fizzBuzzOutput);
                 }
+                //break;
             }
         }
 
         private static bool IsMultiple(int inputNo, int multiplier)
         {
             return (inputNo % multiplier == 0);
+        }
+
+        private static int EnterNo()
+        {
+            Console.WriteLine("Please enter the number you would like to fizz buzz up to");
+            string numInput = Console.ReadLine();
+            int numOutput;
+            while (true)
+            {
+                if (int.TryParse(numInput, out numOutput))
+                {
+                    return numOutput;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer");
+                    numInput = Console.ReadLine();
+                }
+            }
+        }
+        
+        private static StringBuilder FizzBuzzAppend(StringBuilder fizzBuzzCurrent, string newWord, int currentNo ,int multiplier)
+        {
+            if (IsMultiple(currentNo, multiplier))
+            {
+                switch (multiplier)
+                {
+                    default:
+                        fizzBuzzCurrent.Append(newWord);
+                        break;
+                    case 11:
+                        fizzBuzzCurrent = new StringBuilder();
+                        fizzBuzzCurrent.Append(newWord);
+                        break; 
+                    case 13:
+                        fizzBuzzCurrent.Insert(BIndex(fizzBuzzCurrent), newWord);
+                        break;
+                    case 17:
+                        //reverse order of fizzes, buzzes etc.
+                        fizzBuzzCurrent = CapitalWordReverser(fizzBuzzCurrent);
+                        break;
+                }
+            }
+            return fizzBuzzCurrent;
+        }
+
+        private static int BIndex(StringBuilder fizzBuzzCurrent)
+        //This finds the index of the first capital B in the input StringBuilder
+        {
+            if (Array.IndexOf(fizzBuzzCurrent.ToString().ToCharArray(),'B') != -1)
+            {
+                return Array.IndexOf(fizzBuzzCurrent.ToString().ToCharArray(), 'B');
+            }
+            return fizzBuzzCurrent.Length;
+        }
+
+        private static StringBuilder CapitalWordReverser(StringBuilder fizzBuzzCurrent)
+        {
+            StringBuilder reversedString = new StringBuilder();
+            return reversedString;
         }
     }
 }
